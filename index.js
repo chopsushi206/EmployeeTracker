a7c7487114521a39b2ca78435dd95a61b811eda3
LiHua Anderson <lihua.anderson@gmail.com>
2021-04-25 13:01:09 -0700

    adding script to index.js to connect to server and added requirement variables

---
+++ index.js
@@ -0,0 +1,39 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+const consoleTable = require("console.table");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "employee_trackerDB",
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  start();
+});
+
+const start = () => {};
+inquirer
+  .prompt({
+    name: "action",
+    type: "list",
+    message: "What would you like to do?",
+    choices: [
+      "View All Employees",
+      "View All Employees By Department",
+      "View All Employees By Manager",
+      "Add Employee",
+      "Remove Employee",
+      "Update Employee Role",
+      "Update Employee Manager",
+      "View All Roles",
+      "View Utilized Budget By Department",
+      "Exit",
+    ],
+  })
+  .then((answer) => {
+    console.log(answer);
+  });
